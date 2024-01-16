14038acb0cf8e2cd73e43ea37379c1ad9b42964b
ATregubovich-2022 <326632andrey@gmail.com>
2024-01-16 21:48:10 +0700

    Realised disappearance of figures

--- objects.py
+++ objects.py
@@ -1,14 +1,22 @@
 import pygame
+from methods import make_radius
 
 
 class Board(pygame.sprite.Group):
     # создание поля
-    def __init__(self, width, height, screen):
+    def __init__(self, width: int, height: int, screen: pygame.Surface, field: str):
         super().__init__()
         self.screen = screen
         self.width = width
         self.height = height
-        self.field = [[None] * width for _ in range(height)]
+        field = [list(map(int, row.split())) for row in field.split('\n')]
+        self.radius, self.colors = make_radius(field)
+        assert len(field) == self.height and len(field[0]) == self.width, "Размеры поля и карты отличаются"
+        self.field = []
+        for i in range(len(field)):
+            self.field.append([])
+            for j in range(len(field[i])):
+                self.field[i].append((StopCell((i, j), (100, 100, 100), self) if field[i][j] == 1 else None))
         self.left = 10
         self.top = 10
         self.cell_size = 30
@@ -19,10 +27,30 @@ class Board(pygame.sprite.Group):
         self.top = top
         self.cell_size = cell_size
 
-    def make_field(self):
+    def update_field(self):
         self.field = [[None] * self.width for _ in range(self.height)]
         for sprite in self.sprites():
-            self.field[sprite.row + sprite.shape.row][sprite.col + sprite.shape.col] = sprite
+            if type(sprite) is MovingCell:
+                self.field[sprite.row + sprite.shape.row][sprite.col + sprite.shape.col] = sprite
+            else:
+                self.field[sprite.row][sprite.col] = sprite
+
+    def check(self):
+        colors_filled = {}
+        for i in range(len(self.field)):
+            for j in range(len(self.field[i])):
+                if type(self.field[i][j]) is MovingCell:
+                    colors_filled[self.radius[i][j]] = colors_filled.get(self.radius[i][j], 0) + 1
+        for i in range(2, len(self.colors) + 2):
+            if colors_filled.get(i, 0) == self.colors[i]:
+                self.delete(i)
+                return
+
+    def delete(self, color):
+        for i in range(len(self.field)):
+            for j in range(len(self.field[i])):
+                if self.radius[i][j] == color:
+                    self.field[i][j].kill()
 
     def render(self):
         for row in range(self.height):
@@ -35,10 +63,11 @@ class Board(pygame.sprite.Group):
 
 
 class Shape(pygame.sprite.Group):
-    def __init__(self, struct: list, color: tuple, coords: tuple, board: Board):
+    def __init__(self, struct: list, color: tuple, coords: tuple, direction: int, board: Board):
         super().__init__()
         self.color = color
         self.board = board
+        self.direction = direction
         self.row, self.col = coords
         self.struct = []
         for i in range(len(struct)):
@@ -48,7 +77,7 @@ class Shape(pygame.sprite.Group):
                 if struct[i][j] == 0:
                     self.struct[i].append(None)
                 elif struct[i][j] == 1:
-                    self.struct[i].append(Cell((i, j), self.color, self, self.board))
+                    self.struct[i].append(MovingCell((i, j), self.color, self, self.board))
                     self.add(self.struct[i][j])
         self.width = len(self.struct[0])
         self.height = len(self.struct)
@@ -57,8 +86,10 @@ class Shape(pygame.sprite.Group):
         if args[0] == 'm':
             self.move(args[1])
 
-    def move(self, direction):
+    def move(self, direction=None):
         # функция обновляющая координаты фигуры
+        if not direction:
+            direction = self.direction
         for sprite in self.sprites():
             if sprite.move(direction) is False:
                 return False
@@ -73,24 +104,35 @@ class Shape(pygame.sprite.Group):
 
 
 class Cell(pygame.sprite.Sprite):
-    def __init__(self, coords: tuple, color: tuple, shape: Shape, board: Board):
-        super().__init__(shape, board)
+    def __init__(self, coords: tuple, color: tuple, board: Board):
+        super().__init__(board)
         self.row, self.col = coords
         self.color = color
-        self.shape = shape
         self.board = board
         self.board.add(self)
-        self.board.field[self.row + self.shape.row][self.col + self.shape.col] = self
 
-    def update(self, *args):
-        if args[0] == 'm':
-            self.move(args[1])
+    def render(self):
+        pygame.draw.rect(self.board.screen, self.color, (
+            self.board.left + self.col * self.board.cell_size,
+            self.board.top + self.row * self.board.cell_size,
+            self.board.cell_size, self.board.cell_size))
+
+
+class StopCell(Cell):
+    pass
+
+
+class MovingCell(Cell):
+    def __init__(self, coords: tuple, color: tuple, shape: Shape, board: Board):
+        super().__init__(coords, color, board)
+        self.shape = shape
+        self.board.field[self.row + self.shape.row][self.col + self.shape.col] = self
+        shape.add(self)
 
     def move(self, direction):
         assert direction in range(4), "Направление движения должно быть в [0; 3]"
         try:
             if direction == 0:
-                print(self.shape.row + self.row, self.shape.col + self.col)
                 if self.shape.row + self.row == 0:
                     return False
                 if self.board.field[self.shape.row + self.row - 1][self.shape.col + self.col] and \
